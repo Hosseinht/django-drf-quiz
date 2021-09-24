1787ad9463e2583f0d2f57c23a7a2f7e5023c24e
Ryan Taheri <sailantht@gmail.com>
2021-09-24 16:08:08 +0330

    1.Models

---
+++ quiz/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+# Many to one
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quizzes(models.Model):
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizzes")
+        ordering = ['id']
+
+    # foreignkey. one quiz can have many category
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255, default=_("New Quiz"), verbose_name=_("Quiz Title"))
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Updated(models.Model):
+    date_updated = models.DateTimeField(verbose_name=_("Last Updated"), auto_now=True)
+
+    class Meta:
+        abstract = True
+        # common information in multiple tables
+        # when we update a Question/Answer we want to record that it has been update. we want to keep track
+
+
+class Question(Updated):
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ['id']
+
+    SCALE = (
+        (0, _('Fundamental')),
+        (1, _('Beginner')),
+        (2, _('Intermediate')),
+        (3, _('Advanced')),
+        (4, _('Expert')),
+    )
+
+    TYPE = (
+        (0, _('Multiple Choice')),
+    )
+
+    quiz = models.ForeignKey(Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    technique = models.IntegerField(choices=TYPE, default=0, verbose_name=_('Type of Question'))
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    difficulty = models.IntegerField(choices=SCALE, default=0, verbose_name=_('Difficulty'))
+    date_updated = models.DateTimeField(verbose_name=_("Date Created"), auto_now_add=True)
+    is_active = models.BooleanField(default=False, verbose_name=_("Active Status"))
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Answer(Updated):
+    class Meta:
+        verbose_name = _("Answer")
+        verbose_name_plural = _("Answers")
+        ordering = ['id']
+
+    question = models.ForeignKey(Question, related_name='answer', on_delete=models.DO_NOTHING)
+    answer_text = models.CharField(max_length=255, verbose_name=_("Answer Text"))
+    is_right = models.BooleanField(default=False)
