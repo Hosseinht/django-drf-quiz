3d9b559e7f9647d9ed68048a07ccd188e215d4d3
Ryan Taheri <sailantht@gmail.com>
2021-09-25 17:02:44 +0330

    4.Add React

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <React.Fragment>
+            <div><h1>Hello There</h1></div>
+        </React.Fragment>
+    );
+}
+
+export default App;
